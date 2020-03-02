3793a0bdbbb97c6d84360519491d10047e314b3c
apostola <alexei.postolachi@evozon.com>
2020-03-02 17:47:19 +0200

    Hydrator get and set

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use ReallyOrm\Test\Entity\User;
+use ReallyOrm\Test\Hydrator\Hydrator;
+
+$user = new User();
+$user->setName('ciwawa');
+$user->setEmail('email');;
+
+$hydrator = new Hydrator();
+
+//var_dump($hydrator->extract($user));
+
+$obj = $hydrator->hydrate("ReallyOrm\Test\Entity\User", ["name" => "ion", "email" => "123"]);
+
+var_dump($obj->getName());
+var_dump($obj->getEmail());
